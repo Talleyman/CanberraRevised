8e6d25c1388a5b495b9a2fea857be57d0ab3b8f3
Talleyman <Talleyman@users.noreply.github.com>
2015-01-22 15:05:25 -0500

    Finished the rest of the code
    Will do some testing once I get access to CLI again.

--- CanberraRevised.py
+++ CanberraRevised.py
@@ -3,9 +3,9 @@
 #Stephen Talley
 #Date started: January 17, 2015
 import csv
-import getopt, sys
+import getopt, sys, itertools
 try:
-    import numpy
+    import numpy as np
 except ImportError:
     print "Cannot import numpy. Please install numpy and try again.\n"
 try:
@@ -13,7 +13,7 @@ try:
 except ImportError:
     print "Cannot import mlpy. Please install mlpy and try again.\n"
     
-def Doc():
+def Doc():#Function to display the help menu as needed
     print "Canberra Distance help menu:\n\n"
     print "Use -v or -verbose to trigger verbose mode.\n"
     print "Use -h or -help to access this menu.\n"
@@ -34,7 +34,7 @@ def GetArgs():#Collects the arguments for the program
     outputsep="comma"
     outFile="Canberra_Results"
     #Loop through command line arguments to get file names and specifications
-    for o in ops:
+    for o, a in ops:
         if o in ("-v","verbose"):
             verbose=True
         elif o in ("-h","help"):
@@ -42,26 +42,81 @@ def GetArgs():#Collects the arguments for the program
                 print "Loading help menu..."
             Doc()
         elif o in ("-f","-inFile"):
-            inFile=str(o)
+            inFile=str(a)
             if verbose:
                 print "Input file to be analyzed is named", inFile
         elif o in ("-s","-inSep"):
-            inputsep=str(o)
+            inputsep=str(a)
             if verbose:
                 print "Input delimiter is set to", inputsep
         elif o in ("-o","-outFile"):
-            outFile=str(o)
+            outFile=str(a)
             if verbose:
                 print "Output file name is specified as", outFile
         elif o in ("-S","-outSep"):
-            outputsep=str(o)
+            outputsep=str(a)
             if verbose:
                 print "Output delimiter is set to", outputsep
+#Verify that the main required variable is actually defined
     try:
         inFile
     except NameError:
         print "No input file found! Input file required for analysis."
-    try:
-        in
-def GetData():
+        Doc()
+    return inFile, inputsep, outputsep, outFile, verbose
+        
+def GetData(inFile, inputsep): #Reads data from csv file and translates it into a multi-dimensional list
+    List1=[]
+    with open(inFile,mode='rb') as data:
+        reader=csv.reader(data, delimiter=inputsep)
+        for row in reader:
+            seq=map(int, row) #Map, in this case, applies the 'int' operator to every element in the row
+            List1.append(seq)
+    reader=None
+    return np.array(List1)
+    
+def pairwise(iterable):
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return itertools.izip(a, b)
+    
+def ChangeDelim(inputsep, outputsep): #Change the written names for delimiters/separators into actual delimiters
+    if inputsep or outputsep not in ("comma", "whitespace", "tab"):
+        print "Delimiter not an acceptable option."
+    else:
+        if inputsep=="comma":
+            inputsep=','
+        elif inputsep=="whitespace":
+            inputsep=' '
+        elif inputsep=='tab':
+            inputsep='\t'
+        if outputsep=="comma":
+            outputsep=','
+        elif outputsep=="whitespace":
+            outputsep=' '
+        elif inputsep=='tab':
+            outputsep='\t'
+    return inputsep, outputsep
+    
+def main(): 
+    inName,inSep,outSep,outName,verbose=GetArgs()
+    data = GetData(inName, inSep)
+    inSep, outSep = ChangeDelim(inSep, outSep)
+    with open(outName, outSep) as csvfile:
+        out=csv.writer(csvfile)
+        out.writerow("First List", "Second List","Distance")
+        for x, y in pairwise(data):
+            results=mlpy.canberra(x,y)
+            out.writerow([data.index(x),data.index(y),results])
+            if verbose:
+                print "Writing to file...\n"
+    print "\nCompleted. Check "+outName+" for results."     
+        
+if __name__=="__main__":
+    main()
+            
+    
+        
+        
+        
     
