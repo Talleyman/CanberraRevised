9541f20c7fc7cacf16106db19605057a0565c86f
Talleyman <stephen522@earthlink.net>
2015-02-26 11:53:58 -0500

    Removed output separator option, fixed pairwise

--- CanberraRevised.py
+++ CanberraRevised.py
@@ -16,7 +16,7 @@ except ImportError:
     sys.exit()
     
 def Doc():#Function to display the help menu as needed
-    print "Canberra Distance help menu:\n\n"
+    print "Canberra Distance help menu:\n"
     print "Use -v or -verbose to trigger verbose mode."
     print "Use -h or -help to access this menu."
     print "Use -f or -inFile to specify the name of the input file (i.e. the file with the ranked lists)."
@@ -26,21 +26,20 @@ def Doc():#Function to display the help menu as needed
     
 def GetArgs():#Collects the arguments for the program
     try:
-        ops, args = getopt.getopt(sys.argv[1:],"vhf:s:o:S:", ["verbose","help","inFile","inSep","outFile","outSep"])
+        ops, args = getopt.getopt(sys.argv[1:],"vhf:s:o:S:", ["verbose","help","inFile","inSep","outFile"])
     except getopt.GetoptError as err:
         print(err)
         sys.exit()
     #Establish default values for arguments
     verbose=False
     inputsep="comma"
-    outputsep="comma"
     outFile="Canberra_Results"
     #Loop through command line arguments to get file names and specifications
     for op, args in ops:
-        if o[0] in ("-v","verbose"):
+        if op in ("-v","verbose"):
             verbose=True
     for op in ops:
-        if o[0] in ("-h","help"):
+        if op in ("-h","help"):
             if verbose:
                 print "Loading help menu..."
             Doc()
@@ -58,10 +57,6 @@ def GetArgs():#Collects the arguments for the program
             outFile=arg
             if verbose:
                 print "Output file name is specified as", outFile
-        if op in ("-S","-outSep"):
-            outputsep=arg
-            if verbose:
-                print "Output delimiter is set to", outputsep
 #Verify that the main required variable is actually defined
     try:
         inFile
@@ -69,7 +64,7 @@ def GetArgs():#Collects the arguments for the program
         print "No input file found! Input file required for analysis."
         Doc()
         sys.exit()
-    return inFile, inputsep, outputsep, outFile, verbose
+    return inFile, inputsep, outFile, verbose
         
 def GetData(inFile, inputsep): #Reads data from csv file and translates it into a multi-dimensional list
     List1=[]
@@ -79,7 +74,7 @@ def GetData(inFile, inputsep): #Reads data from csv file and translates it into 
         else:
             reader=csv.reader(data, delimiter=' ')
         for row in reader:
-            seq=map(int, row) #Map, in this case, applies the 'int' operator to every element in the row
+            seq=map(int, row)
             List1.append(seq)
     reader=None
     return np.array(List1)
@@ -89,8 +84,8 @@ def pairwise(iterable):
     next(b, None)
     return itertools.izip(a, b)
     
-def ChangeDelim(inputsep, outputsep): #Change the written names for delimiters/separators into actual delimiters
-    if inputsep or outputsep not in ("comma", "whitespace", "tab"):
+def ChangeDelim(inputsep): #Change the written names for delimiters/separators into actual delimiters
+    if inputsep not in ("comma", "Comma","COMMA", "whitespace", "Whitespace", "WHITESPACE","tab","Tab","TAB"):
         print "Delimiter not an acceptable option."
     else:
         if inputsep=="comma":
@@ -99,18 +94,12 @@ def ChangeDelim(inputsep, outputsep): #Change the written names for delimiters/s
             inputsep=' '
         elif inputsep=='tab':
             inputsep='\t'
-        if outputsep=="comma":
-            outputsep=','
-        elif outputsep=="whitespace":
-            outputsep=' '
-        elif inputsep=='tab':
-            outputsep='\t'
-    return inputsep, outputsep
+    return inputsep
     
 def main(): 
-    inName,inSep,outSep,outName,verbose=GetArgs()
+    inName,inSep,outName,verbose=GetArgs()
     data = GetData(inName, inSep)
-    inSep, outSep = ChangeDelim(inSep, outSep)
+    inSep = ChangeDelim(inSep)
     with open(outName, 'wb') as csvfile:
         out=csv.writer(csvfile)
         out.writerow("First List", "Second List","Distance")
